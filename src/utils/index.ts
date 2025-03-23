2f27a030fef02d2e41f4c37cc7716a65f3c10a41
zhangli <2475934003@qq.com>
2025-03-23 16:18:08 +0800

    chore: 类型+注释

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
 import fs from 'node:fs'
 
+
+export type ILoadJson = typeof loadJson
+
+/**
+ * 从指定路径加载 JSON 文件并解析为指定类型。
+ *
+ * @template R - 解析后 JSON 数据的类型。
+ * @param {string} path - 要读取的 JSON 文件的路径。
+ * @returns {Promise<R | NodeJS.ErrnoException>} - 一个 Promise，成功时解析为 JSON 数据，失败时返回错误。
+ */
 export const loadJson = <R>(path: string): Promise<R | NodeJS.ErrnoException> => {
     return new Promise((resolve, reject) => {
         fs.readFile(path, 'utf8', (err, data) => {
@@ -11,3 +21,7 @@ export const loadJson = <R>(path: string): Promise<R | NodeJS.ErrnoException> =>
         })
     })
 }
+
+
+const a = loadJson<{ name: string }>('package.json')
+console.log(a.then(res => console.log(res.name)))
